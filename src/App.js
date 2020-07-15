34fa1627af0d9a5d8acdac7378dc61da63bdb5a0
pavinduLakshan <174084F@uom.lk>
2020-07-15 10:24:23 +0530

    initial  commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,138 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+import Grid from '@material-ui/core/Grid';
+import Button from '@material-ui/core/Button';
+import ToDoItem from './ToDoItem';
+import AppBar from '@material-ui/core/AppBar';
+import IconButton from '@material-ui/core/IconButton';
+import ArrowForwardIcon from '@material-ui/icons/ArrowForwardIos';
+import ArrowBackIcon from '@material-ui/icons/ArrowBackIos';
+import Toolbar from '@material-ui/core/Toolbar';
+import { makeStyles } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const useStyles = makeStyles((theme) => ({
+  title: {
+    flexGrow: 1,
+  },
+  appbar: {
+    alignItems: 'center',
+  }
+}));
 
-export default App;
+const themeLight = createMuiTheme({
+  palette: {
+    background: {
+      default: '#D4E0E4',
+    },
+  },
+});
+
+const Pagination = () => {
+  const [list, setList] = useState([]);
+  const classes = useStyles();
+  const [nextToken, setNextToken] = useState(undefined);
+  const [nextNextToken, setNextNextToken] = useState();
+  const [previousTokens, setPreviousTokens] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [isNextBtnDisabled, setNextBtnDisabled] = useState(false);
+  const [nItems, setNItems] = useState(3);
+
+  function handleNItems(e) {
+    setNItems(e.target.value);
+  }
+
+  useEffect(() => {
+    fetch('https://af5xufo4j6.execute-api.us-east-1.amazonaws.com/test/all', {
+      method: 'POST', // *GET, POST, PUT, DELETE, etc.
+      body: JSON.stringify({ LastEvaluatedKey: nextToken }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        if (!data.LastEvaluatedKey) {
+          setNextBtnDisabled(true);
+        }
+        setNextNextToken(data.LastEvaluatedKey);
+        setList(data.Items);
+      })
+      .catch((e) => console.log(e));
+  }, [nextToken]);
+
+  const next = () => {
+    setPreviousTokens((c) => [...c, nextToken]);
+    setNextToken(nextNextToken);
+    setCurrentPage(currentPage + 1);
+  };
+
+  const prev = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1);
+    }
+    if (isNextBtnDisabled) {
+      setNextBtnDisabled(false);
+    }
+    const copy = [...previousTokens];
+    const b = copy.pop();
+    setPreviousTokens(copy);
+    setNextToken(b);
+  };
+  return (
+    <MuiThemeProvider theme={themeLight}>
+    <CssBaseline />
+    
+      <AppBar position="static" className={classes.appbar}>
+        <Toolbar>
+          <Typography variant="h6" className={classes.title}>
+            The LiveRoom Pagination App
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Grid container spacing={0} className="main_container">
+        <Grid item xs={12} sm={12} md={2} lg={2}></Grid>
+        <Grid item xs={12} sm={12} md={8} lg={8} style={{ marginTop: '1.3%' }}>
+          <div id="btn_container">
+            <p style={{ marginRight: '2%' }}>Rows per page</p>
+            <Select value={nItems} disableUnderline onChange={handleNItems}>
+              <MenuItem value={3}>3</MenuItem>
+              <MenuItem value={5}>5</MenuItem>
+              <MenuItem value={10}>10</MenuItem>
+            </Select>
+            <div style={{ flexGrow: 1 }} />
+            <IconButton
+              onClick={prev}
+              className="pagination_btn"
+              disableRipple
+              disableFocusRipple
+              disabled={currentPage === 1}
+            >
+              <ArrowBackIcon />
+            </IconButton>
+            <p id="current_page">current page: {currentPage}</p>
+            <IconButton
+              onClick={next}
+              className="pagination_btn"
+              disableRipple
+              disableFocusRipple
+              disabled={isNextBtnDisabled}
+              style={{ padding: 0 }}
+            >
+              <ArrowForwardIcon />
+            </IconButton>
+          </div>
+          {list.map((item, index) => {
+            return <ToDoItem id={Math.random()} timeout={250 * (index + 1)} name={item.PK} date={item.SK} />;
+          })}
+        </Grid>
+      </Grid>
+    
+ 
+    </MuiThemeProvider>
+  );
+};
+
+export default Pagination;
---
+++ src/ToDoItem.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import MenuList from '@material-ui/core/MenuList';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
+import NotCompletedIcon from '@material-ui/icons/CheckCircleOutline';
+import UndoCompletedIcon from '@material-ui/icons/Undo';
+import MoreVerticalIcon from '@material-ui/icons/MoreVert';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import Typography from '@material-ui/core/Typography';
+import EditIcon from '@material-ui/icons/Edit';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MenuItem from '@material-ui/core/MenuItem';
+import Menu from '@material-ui/core/Menu';
+import Grow from '@material-ui/core/Grow';
+
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'left',
+    color: theme.palette.text.secondary,
+    display: 'flex',
+    justifyContent: 'center',
+    flexDirection: 'row',
+  },
+}));
+
+const ToDoItem = ({ name, date, time, timeout, isFirstRender, isCompleted }) => {
+  const classes = useStyles();
+
+  return (
+    <Grow in={true} key={name} unmountOnExit={true} style={{ transformOrigin: `0 0 0` }} {...{ timeout: timeout }}>
+      <Paper
+        elevation={3}
+        className={classes.paper}
+        style={{
+          color: isCompleted ? 'white' : 'black',
+          marginBottom: '2%',
+          backgroundColor: isCompleted ? '#35C706' : 'white',
+        }}
+      >
+        {/* Todo item details */}
+        <div>
+          <Typography variant="h6">{name}</Typography>
+          <Typography variant="subtitle1">{date}</Typography>
+        </div>
+        <div style={{ flexGrow: 1 }}></div>
+        <img src={`https://avatars.dicebear.com/api/human/${name}.svg`} height="50" width="50" />
+      </Paper>
+    </Grow>
+  );
+};
+
+export default ToDoItem;
