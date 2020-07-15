e524c018352221f67865ea9a67e5304cba7f9110
pavinduLakshan <174084F@uom.lk>
2020-07-15 10:26:47 +0530

    Updates

---
+++ precache-manifest.7b82b01bc2a1ba25b0e09150c59e7539.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b3be79ff257373d0a217e4d05adbe702",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a91675a7322b9dcd66b6",
+    "url": "/static/css/main.2cde41fd.chunk.css"
+  },
+  {
+    "revision": "0f8a99ccb96787dec0aa",
+    "url": "/static/js/2.2bba7d6f.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.2bba7d6f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a91675a7322b9dcd66b6",
+    "url": "/static/js/main.8e6f618c.chunk.js"
+  },
+  {
+    "revision": "562f29f9d586644f74e2",
+    "url": "/static/js/runtime-main.c7f764a5.js"
+  }
+]);
